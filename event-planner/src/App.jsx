b27af72ea85136ae7eb1604cd9fe1a0203ade618
Kimsrun Lim <jvstingamboa@gmail.com>
2024-02-28 21:27:36 -0500

    testing out react components

---
+++ event-planner/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+function App() {
+
+  return (
+    
+    <div>
+
+      <h1>Hello World</h1>3
+      <Navbar></Navbar>
+      <Navbar />
+      
+    </div>
+     
+      
+    
+  )
+}
+
+export default App
---
+++ event-planner/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+function Navbar() { 
+
+    return (
+        <h1>Hello OWrk cs</h1>
+        
+    )
+
+}
+
+export default Navbar
