a244bb7cf49f9841661aa4d5823d9c47ba0e6882
KimsrunLim <ksrunlim@gmail.com>
2024-03-27 18:10:50 -0400

    Rough Login setup
    Need to go through DaisyUI with Justin

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import { Card } from "daisyui";
 
 
-function Login() 
-{
+const Login = () => {
     return (
-        <div>THIS IS Login Component</div>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center',
+                    textAlign: 'center', margin: 'auto', backgroundColor: 'white',
+                    maxWidth: '500px', border: '1px solid red', borderRadius: '30px', padding: '1rem' }}>
+            <form>
+                <div>
+                    <label className="label">
+                        <span className="text-base label-text">Username</span>
+                    </label>
+                    <input type="text" id="username" placeholder="Username/Email"
+                        className="w-full input input-bordered input-primary" />
+                </div>
+                <div>
+                    <label className="label">
+                        <span className="text-base label-text">Password</span>
+                    </label>
+                    <input type="password" id="password" placeholder="Enter Password"
+                        className="w-full input input-bordered input-primary" />
+                </div>
+                <a href="#" className="text-xs text-gray-600 hover:underline hover:text-blue-600">Forget Password?</a>
+                <div>
+                    <button className="btn btn-primary">Login</button>
+                </div>
+                <div className="text-sm text-gray-600">Don't have an account?</div>
+                <div>
+                    <button className="btn btn-warning text">Create account here!</button>
+                </div>
+            </form>
+        </div>
     );
 }
 
--- frontend/src/pages/loginPage.js
+++ frontend/src/pages/loginPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Login from '../components/Login';
 
 function LoginPage() {
     return (
-        <div>Test Test Test</div>
+        <>
+            <Login />
+        </>
     );
 }
 
