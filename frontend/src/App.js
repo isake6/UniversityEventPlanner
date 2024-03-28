49cde2ff29150e68cc6f3f18ebeca33a76e6c677
jvstingamboa <jvstingamboa@gmail.com>
2024-03-28 14:00:13 -0400

    addded tailwind config file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import LoginPage from './pages/loginPage';
 
+
 function App() {
   return (
-    <LoginPage />
+    <div>
+      <LoginPage/>
+    </div>
   );
 }
 
