18958dbdb82bfd7b95ec56601164bd26ef7fe2b2
KimsrunLim <ksrunlim@gmail.com>
2024-03-28 16:49:24 -0400

    Path for create on click works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,16 @@ import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-rou
 import LoginPage from './pages/loginPage';
 import CreateAccountPage from './pages/createAccountPage';
 import CreateEventForm from './components/CreateEventForm';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
-  
       <Routes>
-
-        <Route path="/" index element={<LoginPage />} />
-        <Route path="/create" element={<CreateAccountPage />} />        
-        
+          <Route path="/" index element={<LoginPage />} />
+          <Route path="/create" index element={<CreateAccountPage />} />
       </Routes>
-        
+
     </BrowserRouter>
   );
 }
