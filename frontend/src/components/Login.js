e330404566316d4b8692a58f09cf50372d44096d
KimsrunLim <ksrunlim@gmail.com>
2024-03-28 22:54:50 -0400

    Corrected Login Card (Size needs correcting)

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,42 +3,67 @@ import { Link } from 'react-router-dom';
 
 
 
-function Login ()  {
-    
-
-    
+function Login() {
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center',
-                    textAlign: 'center', margin: 'auto', backgroundColor: 'white',
-                    maxWidth: '500px', borderRadius: '30px', padding: '1rem' }}>
-            <form>
-                <div>
-                    <label className="label">
-                        <span className="text-base label-text">Username</span>
-                    </label>
-                    <input type="text" id="username" placeholder="Username/Email"
-                        className="w-full input input-bordered input-primary" />
+        <div className="flex justify-center items-center h-screen">
+            <div className="w-fit max-w-2xl mx-auto bg-white rounded-xl shadow-md overflow-hidden">
+                <div className="p-5">
+                    {/* Your card content here */}
+                    <h1 className="text-3xl font-bold text-center">Event Login</h1>
+                    <form>
+                        <h3 className="text-base font-bold pt-4">Username</h3>
+                        <input type="text" id="username" placeholder='Username/Email' className=" w-full input input-bordered input-primary"></input>
+
+                        <h3 className="text-sm font-bold">Password</h3>
+                        <input type="password" id="password" placeholder='Username/Email' className="w-full input input-bordered input-primary"></input>
+                    </form>
+
+                    <div className='flex flex-col p-4'>
+                        <button className='btn btn-info'>Login</button>
+                    </div>
+
+                    <div className='flex flex-col'>
+                        <a href='#' className='text-xs text-gray-600 hover:underline hover:text-blue-600 text-center'>Forgot Password?</a>
+                        <a href='#' className='text-xs text-gray-600 hover:underline hover:text-blue-600 text-center'>Create Account</a>
+                    </div>
+                    
                 </div>
-                <div>
-                    <label className="label">
-                        <span className="text-base label-text">Password</span>
-                    </label>
-                    <input type="password" id="password" placeholder="Enter Password"
-                        className="w-full input input-bordered input-primary" />
-                </div>
-                <a href="#" className="text-xs text-gray-600 hover:underline hover:text-blue-600">Forget Password?</a>
-                <div>
-                    <button className="btn btn-primary">Login</button>
-                </div>
-                <div className="text-sm text-gray-600">Don't have an account?</div>
-                <div>
-                <Link to={'/create'} ><button className="btn btn-warning text">Create an account here!</button></Link>
-                   
-                </div>
-            
-            </form>
+            </div>
         </div>
+
+        // <div style={{
+        //     display: 'flex', flexDirection: 'column', alignItems: 'center',
+        //     textAlign: 'center', margin: 'auto', backgroundColor: 'white',
+        //     maxWidth: '500px', borderRadius: '30px', padding: '1rem'
+        // }}>
+        //     <form>
+        //         <div>
+        //             <label className="label">
+        //                 <span className="text-base label-text">Username</span>
+        //             </label>
+        //             <input type="text" id="username" placeholder="Username/Email"
+        //                 className="w-full input input-bordered input-primary" />
+        //         </div>
+        //         <div>
+        //             <label className="label">
+        //                 <span className="text-base label-text">Password</span>
+        //             </label>
+        //             <input type="password" id="password" placeholder="Enter Password"
+        //                 className="w-full input input-bordered input-primary" />
+        //         </div>
+        //         <a href="#" className="text-xs text-gray-600 hover:underline hover:text-blue-600">Forget Password?</a>
+        //         <div>
+        //             <button className="btn btn-primary">Login</button>
+        //         </div>
+        //         <div className="text-sm text-gray-600">Don't have an account?</div>
+        //         <div>
+        //             <Link to={'/create'} ><button className="btn btn-warning text">Create an account here!</button></Link>
+
+        //         </div>
+
+        //     </form>
+        // </div>
     );
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,87 +3,81 @@ import { Link } from 'react-router-dom';
 import CreateEventForm from './CreateEventForm';
 
 function Navbar() {
-  var [_modalState, setModalState] = useState(false);
-
-  function handleModal() 
-  {
-    setModalState(true);
-  }
-
-
 
   return (
-    <div class="navbar bg-warning pb-3">
-      <div class="navbar-start">
-        <div class="dropdown">
-          <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div>
+      <div class="navbar bg-yellow-500 pb-3 fixed">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabindex="0"
+              class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+              <li>
+                <a>Events</a>
+              </li>
+              <li>
+                <a>Parent</a>
+                <ul class="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabindex="0"
-            class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
+          <Link to={'/'}>
+            <button class="btn btn-ghost text-xl">daisyUI</button>
+          </Link>
+        </div>
+        <div class="navbar-center hidden lg:flex">
+          <ul class="menu menu-horizontal px-1">
             <li>
-              <a>Events</a>
+              <a>Item 1</a>
             </li>
             <li>
-              <a>Parent</a>
-              <ul class="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
+              <details>
+                <summary>Parent</summary>
+                <ul class="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </details>
             </li>
             <li>
-              <a>Item 3</a>
+              <button>fsfsd</button>
             </li>
           </ul>
         </div>
-        <Link to={'/'}>
-          <button class="btn btn-ghost text-xl">daisyUI</button>
-        </Link>
-      </div>
-      <div class="navbar-center hidden lg:flex">
-        <ul class="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul class="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <button onclick={handleModal}>fsfsd</button>
-          </li>
-        </ul>
-      </div>
-      <div class="navbar-end">
-        <a class="btn">Button</a>
+        <div class="navbar-end">
+          <a class="btn">Button</a>
+        </div>
       </div>
     </div>
   );
