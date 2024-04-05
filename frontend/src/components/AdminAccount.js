92a68a6f970d483723a57c585bc6e704d181117c
KimsrunLim <ksrunlim@gmail.com>
2024-04-05 19:28:06 -0400

    Small adjustment, gonna move on to add event containers

--- frontend/src/components/AdminAccount.js
+++ frontend/src/components/AdminAccount.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 
-
 const AdminAccount = () => {
-
+    var name = "John Doe";
 
     return (
         <>
-            <div className="flex justify-center items-center h-screen pt-28 bg-white w-auto mx-10">
-                Test
+            <div className='h-screen bg-white'>
+                <div className='flex justify-center align-middle pt-20 text-4xl text-black font-bold'>
+                    {name}
+                </div>
             </div>
         </>
     );
