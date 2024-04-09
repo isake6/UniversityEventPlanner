71191644d9ba2e63bd8de748c5a8b61194766c45
Justin Gamboa <jvstingamboa@gmail.com>
2024-04-09 18:52:54 -0400

    changed id to fninsh event creation form

--- frontend/src/components/CreateEventForm.jsx
+++ frontend/src/components/CreateEventForm.jsx
@@ -10,34 +10,59 @@ const createEventForm = () => {
         <h2>Create your event here! </h2>
       </div>
 
-      <div className="flex justify-center items-center min-h-screen">
+      <div className="flex justify-center items-center min-h-screen ">
         {/* This line has been updated */}
-        <div className="space-y-4 max-w-md w-full">
+        <div className="space-y-4 max-w-md w-full  shadow-xl ">
           {/* Added for form styling */}
-          <label className="input input-bordered flex items-center gap-2">
+
+          <label
+            className="input input-bordered flex items-center gap-2"
+            id="event_name"
+          >
             Event Name
             <input type="text" placeholder="Knight World" />
           </label>
+
           <label className="input input-bordered flex items-center gap-2">
-            Description
-            <input
-              type="text-area"
-              placeholder="It's a world of knights!"
-            />{' '}
+            Category
+            <input type="input" id="category" placeholder="Tailgate" />{' '}
             {/* Changed type to email */}
           </label>
+
           <label className="input input-bordered flex items-center gap-2">
-            Event Type
-            <input type="dropdown" id="eventType" placeholder="Private" />
+            Event Time
+            <input type="text" id="time" placeholder="2:00pm" />
           </label>
+
           <label className="input input-bordered flex items-center gap-2">
-            <input type="text" className="grow" placeholder="Search" />
-            <span className="badge badge-info">Optional</span>
+            Event Description
+            <input
+              type="text"
+              id="description"
+              placeholder="Football Fall Tailgate!"
+            />
           </label>
+
           <label className="input input-bordered flex items-center gap-2">
-            <input type="text" className="grow" placeholder="Search" />
-            <span className="badge badge-info">Optional</span>
+            Event Location
+            <input
+              type="text"
+              id="location"
+              placeholder="Football Fall Tailgate!"
+            />
           </label>
+
+          <label className="input input-bordered flex items-center gap-2">
+            Phone Number
+            <input type="text" id="phone" placeholder="123-456-7890" />
+          </label>
+
+          <label className="input input-bordered flex items-center gap-2">
+            Email
+            <input type="text" id="email" placeholder="ucfknights@ucf.edu" />
+          </label>
+
+          <button className="btn btn-info">Submit</button>
         </div>
       </div>
     </>
