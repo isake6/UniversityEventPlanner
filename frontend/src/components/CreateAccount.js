00505dde8c2ede8b683b491ab4fc8de482c34a7f
Justin Gamboa <jvstingamboa@gmail.com>
2024-04-11 09:52:56 -0400

    added local session activity using local storage

--- frontend/src/components/CreateAccount.js
+++ frontend/src/components/CreateAccount.js
@@ -2,7 +2,6 @@ import React from 'react';
 import axios from 'axios';
 
 const CreateAccount = () => {
-
   const handleSubmit = async (event) => {
     event.preventDefault();
     console.log('Form submitted. Awaiting response...');
@@ -20,9 +19,12 @@ const CreateAccount = () => {
     }
 
     try {
-      const response = await axios.post('https://somethingorother.xyz/create', { firstName, lastName, email, password, role }, { withCredentials: true });
+      const response = await axios.post(
+        'https://somethingorother.xyz/create',
+        { firstName, lastName, email, password, role },
+        { withCredentials: true }
+      );
       console.log('Response:', response.data);
-
     } catch (error) {
       // Log the error message
       if (error.response) {
@@ -36,53 +38,112 @@ const CreateAccount = () => {
         console.error('Error', error.message);
       }
     }
-  }
+  };
 
   return (
     <div>
       <div className="flex items-center h-screen pt-28">
         <div className="w-1/3 h-fit max-w-xl m-auto bg-white rounded-xl shadow-2xl overflow-hidden flex flex-col justify-center border border-yellow-500">
           <div className="p-5">
-            <h1 className="text-3xl font-bold text-center text-black">Create Account</h1>
+            <h1 className="text-3xl font-bold text-center text-black">
+              Create Account
+            </h1>
             <form onSubmit={handleSubmit}>
-              <h3 className="text-base font-bold pt-3 text-gray-600">First Name</h3>
-              <input type="text" id="firstName" placeholder='First Name' className=" w-full input input-bordered input-primary"></input>
+              <h3 className="text-base font-bold pt-3 text-gray-600">
+                First Name
+              </h3>
+              <input
+                type="text"
+                id="firstName"
+                placeholder="First Name"
+                className=" w-full input input-bordered input-primary"
+              ></input>
 
-              <h3 className="text-base font-bold pt-3 text-gray-600">Last Name</h3>
-              <input type="text" id="lastName" placeholder='Last Name' className="w-full input input-bordered border-yellow-500"></input>
+              <h3 className="text-base font-bold pt-3 text-gray-600">
+                Last Name
+              </h3>
+              <input
+                type="text"
+                id="lastName"
+                placeholder="Last Name"
+                className="w-full input input-bordered border-yellow-500"
+              ></input>
 
               <h3 className="text-base font-bold pt-3 text-gray-600">Email</h3>
-              <input type="text" id="email" placeholder='Email' className=" w-full input input-bordered border-yellow-500"></input>
+              <input
+                type="text"
+                id="email"
+                placeholder="Email"
+                className=" w-full input input-bordered border-yellow-500"
+              ></input>
 
-              <h3 className="text-base font-bold pt-3 text-gray-600">Password</h3>
-              <input type="password" id="password" placeholder='Password' className=" w-full input input-bordered border-yellow-500"></input>
+              <h3 className="text-base font-bold pt-3 text-gray-600">
+                Password
+              </h3>
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                className=" w-full input input-bordered border-yellow-500"
+              ></input>
 
-              <h3 className="text-base font-bold pt-3 text-gray-600">Confirm Password</h3>
-              <input type="password" id="passwordConfirm" placeholder='Confirm Password' className="w-full input input-bordered border-yellow-500"></input>
+              <h3 className="text-base font-bold pt-3 text-gray-600">
+                Confirm Password
+              </h3>
+              <input
+                type="password"
+                id="passwordConfirm"
+                placeholder="Confirm Password"
+                className="w-full input input-bordered border-yellow-500"
+              ></input>
 
               <div className="relative">
                 <h3 className="text-base font-bold mt-2 text-gray-800">Role</h3>
                 <select className="w-full input input-bordered input-primary border border-gray-300 appearance-none">
                   <option value="superAdmin">Super Admin</option>
                   <option value="admin">Admin</option>
-                  <option selected value="student">Student</option>
+                  <option selected value="student">
+                    Student
+                  </option>
                 </select>
                 <div className="absolute inset-y-0 right-0 flex items-center px-2 pt-5 pointer-events-none">
-                  <svg className="w-5 h-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+                  <svg
+                    className="w-5 h-5 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M19 9l-7 7-7-7"
+                    ></path>
                   </svg>
                 </div>
               </div>
 
-              <div className=' w-full py-6 flex flex-col m-auto'>
-                <button type="submit" className='btn btn-info font-bold text-xl bg-yellow-500'>Sign Up</button>
+              <div className=" w-full py-6 flex flex-col m-auto">
+                <button
+                  type="submit"
+                  className="btn btn-info font-bold text-xl bg-yellow-500"
+                >
+                  Sign Up
+                </button>
               </div>
             </form>
 
-            <div className='pt-3 flex flex-col'>
-              <div className='m-auto'>
-                <a className='text-base text-gray-600'>Already have an account? </a>
-                <a href='/' className='text-base text-gray-600 hover:underline hover:text-blue-600 text-center'>Login</a>
+            <div className="pt-3 flex flex-col">
+              <div className="m-auto">
+                <a className="text-base text-gray-600">
+                  Already have an account?{' '}
+                </a>
+                <a
+                  href="/"
+                  className="text-base text-gray-600 hover:underline hover:text-blue-600 text-center"
+                >
+                  Login
+                </a>
               </div>
             </div>
           </div>
@@ -90,6 +151,6 @@ const CreateAccount = () => {
       </div>
     </div>
   );
-}
+};
 
 export default CreateAccount;
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,62 +1,120 @@
 import React from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        console.log('Form submitted. Awaiting response...');
+  const navigate = useNavigate();
 
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log('Form submitted. Awaiting response...');
 
-        try {
-        
-            const response = await axios.post('https://somethingorother.xyz/login', { email, password }, { withCredentials: true });
-            console.log('Response:', response.data);
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-        } catch (error) {
-            // Log the error message
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                console.error('Error message:', error.response.data);
-            } else if (error.request) {
-                // The request was made but no response was received
-                console.error('No response received:', error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.error('Error', error.message);
-            }
-        }
+    try {
+      const response = await axios.post(
+        'https://somethingorother.xyz/login',
+        { email, password },
+        { withCredentials: true }
+      );
+      console.log('Response:', response.data);
+
+      // Renamed "email" from the response to "emailResponse" to avoid shadowing
+      const {
+        id,
+        first_name,
+        last_name,
+        role,
+        email: emailResponse,
+        university_id,
+      } = response.data;
+
+      console.log('User ID:', id);
+
+      // Use the renamed variable "emailResponse" here
+      const userSessionData = JSON.stringify({
+        id,
+        first_name,
+        last_name,
+        role,
+        email: emailResponse,
+        university_id,
+      });
+
+      localStorage.setItem('userSession', userSessionData);
+
+      navigate('/home');
+    } catch (error) {
+      // Log the error message
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        console.error('Error message:', error.response.data);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error('No response received:', error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error('Error', error.message);
+      }
     }
+  };
 
-    return (
-        <div className="flex items-center h-screen">
-            <div className="w-1/3 h-fit max-w-xl mx-auto bg-white rounded-xl shadow-2xl overflow-hidden flex flex-col justify-center border border-yellow-500">
-                <div className="p-5">
-                    <h1 className="text-3xl font-bold text-center text-black">Event Login</h1>
-                    <form onSubmit={handleSubmit}>
-                        <h3 className="text-base font-bold pt-10 text-gray-600">Email</h3>
-                        <input type="text" id="email" placeholder='Email' className=" w-full input input-bordered input-primary"></input>
+  return (
+    <div className="flex items-center h-screen">
+      <div className="w-1/3 h-fit max-w-xl mx-auto bg-white rounded-xl shadow-2xl overflow-hidden flex flex-col justify-center border border-yellow-500">
+        <div className="p-5">
+          <h1 className="text-3xl font-bold text-center text-black">
+            Event Login
+          </h1>
+          <form onSubmit={handleSubmit}>
+            <h3 className="text-base font-bold pt-10 text-gray-600">Email</h3>
+            <input
+              type="text"
+              id="email"
+              placeholder="Email"
+              className=" w-full input input-bordered input-primary"
+            ></input>
 
-                        <h3 className="text-sm font-bold text-gray-600 pt-5">Password</h3>
-                        <input type="password" id="password" placeholder='Password' className="w-full input input-bordered input-primary"></input>
+            <h3 className="text-sm font-bold text-gray-600 pt-5">Password</h3>
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              className="w-full input input-bordered input-primary"
+            ></input>
 
-                        <div className=' w-full py-5 flex flex-col m-auto'>
-                            <button type="submit" className='btn btn-info font-bold text-xl bg-yellow-500'>Login</button>
-                        </div>
-                    </form>
-
-                    <div className='pt-3 flex flex-col'>
-                        <a href='#' className='text-base text-gray-600 hover:underline hover:text-blue-600 text-center'>Forgot Password?</a>
-                        <div className='m-auto'>
-                            <a className='text-base text-gray-600'>Don't have an account? </a>
-                            <a href='/create' className='text-base text-gray-600 hover:underline hover:text-blue-600 text-center'>Create Account</a>
-                        </div>
-                    </div>
-                </div>
+            <div className=" w-full py-5 flex flex-col m-auto">
+              <button
+                type="submit"
+                className="btn btn-info font-bold text-xl bg-yellow-500"
+              >
+                Login
+              </button>
             </div>
+          </form>
+
+          <div className="pt-3 flex flex-col">
+            <a
+              href="#"
+              className="text-base text-gray-600 hover:underline hover:text-blue-600 text-center"
+            >
+              Forgot Password?
+            </a>
+            <div className="m-auto">
+              <a className="text-base text-gray-600">Don't have an account? </a>
+              <a
+                href="/create"
+                className="text-base text-gray-600 hover:underline hover:text-blue-600 text-center"
+              >
+                Create Account
+              </a>
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Login;
