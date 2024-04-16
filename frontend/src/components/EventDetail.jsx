917f0dde476a0be17a2fba1b62ad482e2e205fb0
isake6 <isaiahnhan@gmail.com>
2024-04-16 17:08:27 -0400

    Update EventDetail.jsx
    Added Host RSO info to EventDetail.jsx, if the event isn't public.

--- frontend/src/components/EventDetail.jsx
+++ frontend/src/components/EventDetail.jsx
@@ -153,6 +153,11 @@ const EventDetail = () => {
       <div className="py-12 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-center">{event.name}</h1>
+          {event.category !== "public" && (
+            <p className="text-lg text-gray-600 text-center mt-2">
+              Hosted by: {event.rso_name}
+            </p>
+          )}
           <p className="text-lg text-gray-600 text-center mt-2">
             {event.location} - {event.category}
           </p>
